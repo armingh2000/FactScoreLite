26fadfbf37bb0c0c5b174170aa9c17e8f13e6149
armingh2000 <armingh379@gmail.com>
2024-04-02 23:07:22 -0400

    update openai_agent.py
    move api configs to configs.py

--- FactScoreLite/configs.py
+++ FactScoreLite/configs.py
@@ -2,3 +2,9 @@ import importlib.resources
 
 # Path to the data file within the package
 demons_path = importlib.resources.files("FactScoreLite") / "data" / "demons.json"
+
+
+# OpenAI API
+max_tokens = 1024
+temp = 0.7
+model_name = "gpt-4-turbo-preview"
