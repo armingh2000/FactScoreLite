43981ba9d2aebae5422634ba03d0abe2699fdc01
armingh2000 <armingh379@gmail.com>
2024-04-11 16:58:27 -0400

    update configs.py

--- FactScoreLite/configs.py
+++ FactScoreLite/configs.py
@@ -1,10 +1,16 @@
 import importlib.resources
 
 # Path to the data file within the package
-demons_path = importlib.resources.files("FactScoreLite") / "data" / "demons.json"
+data_path = importlib.resources.files("FactScoreLite") / "data"
 
+# Path to the data file within the package
+demons_path = data_path / "demons.json"
 
 # OpenAI API
 max_tokens = 1024
 temp = 0.7
 model_name = "gpt-4-turbo-preview"
+
+# Database path
+facts_db_path = "facts.json"
+decisions_db_path = "decisions.json"
