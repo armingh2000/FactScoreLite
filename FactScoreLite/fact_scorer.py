f159b66e6f91bfcff7c31422a27c923104dbb997
armingh2000 <armingh379@gmail.com>
2024-04-18 20:43:22 -0400

    feat: add demons to fact score prompt
    demons are located in data/fact_scorer_demons.json

--- FactScoreLite/fact_scorer.py
+++ FactScoreLite/fact_scorer.py
@@ -1,11 +1,52 @@
 import string
 from .openai_agent import OpenAIAgent
+from . import configs
+import json
+import random
 
 
 class FactScorer:
     def __init__(self):
+        # Examples (demonstrations) that is used in prompt generation
+        self.demons = self.load_demons()
         # To interact with OpenAI APIs
         self.openai_agent = OpenAIAgent()
+
+    def load_demons(self):
+        """
+        Load examples (demonstrations) from a JSON file.
+        This will be used in the prompt generation.
+
+        Returns:
+            list: A list of examples (demonstrations).
+        """
+        with open(configs.fact_scorer_demons_path, "r") as file:
+            demons = json.load(file)
+
+        return demons
+
+    def get_instructions(self) -> str:
+        """
+        Prepare instructions for the prompt generation.
+        Instructions include the examples given in the fact_scorer_demons.json file.
+
+        Returns:
+            str: The instructions for the prompt generation.
+        """
+
+        instructions = "Evaluate the truthfulness of the statement based solely on the provided context and provide the reason for your decision.\n\n"
+        instructions += "Instruction:\nOnly consider the statement true if it can be directly verified by the information in the context. If the information in the statement cannot be found in the context or differs from it, label it as false.\n\n"
+        true_example = self.demons[0]
+        false_example = random.choice(self.demons[1:])
+
+        for demon in [true_example, false_example]:
+            instructions += f"Context:\n{demon['knowledge_source']}\n"
+            instructions += f"Statement:\n{demon['fact']} True or False?\n"
+            instructions += f"Output:\n{demon['is_supported']}\n\n"
+            # TODO: add reason (+change parsing)
+            # instructions += f"Reason:\n{demon['reason']}\n\n"
+
+        return instructions
 
     def get_score(self, facts: list, knowledge_source: str) -> list:
         """
@@ -26,15 +67,10 @@ class FactScorer:
             atom = atom.strip()
 
             # Prompt that will be sent to GPT
-            prompt = "Answer the question based on the given context.\n\n"
-            prompt += f"Context:\n{knowledge_source}"
-
-            if not prompt[-1] in string.punctuation:
-                prompt += "."
-
-            prompt += "\n\n"
-
-            prompt += f"Input: {atom} True or False?\nOutput:\n"
+            prompt = self.get_instructions()
+            prompt += f"Context:\n{knowledge_source}\n"
+            prompt += f"Statement:\n{atom} True or False?\n"
+            prompt += "Output:\n"
 
             output = self.openai_agent.generate(prompt)
 
